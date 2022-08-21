642c2f5845d75dd966101e4a65b905af4f4b1d37
Engineer <itengineer@live.ru>
2022-08-22 00:23:19 +0300

    add: interface IInternetThing

---
+++ Magals.DevicesControl.SDKStandart/Interfeces/IInternetThing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magals.DevicesControl.SDKStandart.Interfeces
+{
+	public interface IInternetThing : IDevice
+	{
+		void SendCommand(string command);
+		object GetData(string command);
+	}
+}
