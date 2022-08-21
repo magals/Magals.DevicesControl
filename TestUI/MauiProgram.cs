cbc3571bc2d1cf8b22bc2e9bfa25da67d7d7f171
Engineer <itengineer@live.ru>
2022-08-22 00:22:01 +0300

    add: CommunityToolkit.Maui in project TestUI

--- TestUI/MauiProgram.cs
+++ TestUI/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace TestUI
+﻿using CommunityToolkit.Maui;
+
+namespace TestUI
 {
 	public static class MauiProgram
 	{
@@ -7,6 +9,7 @@
 			var builder = MauiApp.CreateBuilder();
 			builder
 				.UseMauiApp<App>()
+				.UseMauiCommunityToolkit()
 				.ConfigureFonts(fonts =>
 				{
 					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
