24301a764117297726745f75afa05793e70ac8b9
magals <itengineer@live.ru>
2022-11-13 23:24:44 +0300

    add: GpioSettingsAttribute, ILed

---
+++ src/Magals.DevicesControl.SDKStandart/Attributes/GpioSettingsAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Magals.DevicesControl.SDKStandart.Attributes
+{
+  [DataContract]
+  [AttributeUsage(AttributeTargets.Class)]
+  public class GpioSettingsAttribute : Attribute
+  {
+    public bool FlagDefault = true;
+    public GpioSettingsAttribute()
+    {
+
+    }
+
+    public GpioSettingsAttribute(int[] outputHigh,
+                                 int[] outputLow,
+                                 int[] input,
+                                 int[] inputPullDown,
+                                 int[] InputPullUp)
+    {
+      this.outputHigh = outputHigh;
+      this.outputLow = outputLow;
+      this.input = input;
+      this.inputPullDown = inputPullDown;
+      this.InputPullUp = InputPullUp;
+
+      FlagDefault = false;
+    }
+
+    [DataMember]
+    public int[] outputHigh { get; set; } = new int[0];
+    [DataMember]
+    public int[] outputLow { get; set; } = new int[0];
+
+    [DataMember]
+    public int[] input { get; set; } = new int[0];
+
+    [DataMember]
+    public int[] inputPullDown { get; set; } = new int[0];
+
+    [DataMember]
+    public int[] InputPullUp { get; set; } = new int[0];
+
+  }
+}
---
+++ src/Magals.DevicesControl.SDKStandart/Interfeces/GPIO/ILed.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magals.DevicesControl.SDKStandart.Interfeces.GPIO
+{
+  public interface ILed : IDevice
+  {
+    void OnOff(bool state);
+  }
+}
