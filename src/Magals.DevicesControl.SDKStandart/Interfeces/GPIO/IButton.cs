2ae71076e46757e4c3e9c21899b90a3c55756fb3
magals <itengineer@live.ru>
2022-11-22 21:35:31 +0300

    add: IButton

---
+++ src/Magals.DevicesControl.SDKStandart/Interfeces/GPIO/IButton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magals.DevicesControl.SDKStandart.Interfeces.GPIO
+{
+	internal interface IButton : IDevice
+	{
+		void Push();
+	}
+}
