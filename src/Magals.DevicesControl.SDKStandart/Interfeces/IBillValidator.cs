56d0c8221e7ab7a5fbef6eaf6d0a3f83b104bd48
magals <itengineer@live.ru>
2024-06-28 01:19:51 +0300

    add: ICoinDispenser, IBillValidator

---
+++ src/Magals.DevicesControl.SDKStandart/Interfeces/IBillValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magals.DevicesControl.SDKStandart.Interfeces
+{
+    public interface IBillValidator : IDevice
+    {
+        void Enable();
+        void Disable();
+
+        Dictionary<string, int> GetStatistics();
+        void ResetStatistics();
+        void EmptyStored();
+
+        event Action<(int denomination, string currency, int codecurrency)> NoteInEscrow;
+        event Action<(int denomination, string currency, int codecurrency)> NoteInStored;
+        event Action NoteReject;
+    }
+}
---
+++ src/Magals.DevicesControl.SDKStandart/Interfeces/ICoinDispenser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magals.DevicesControl.SDKStandart.Interfeces
+{
+    public interface ICoinDispenser : IDevice
+    {
+        void DispanseCoin(int denomination, string currency, int amount, bool autoOutDenomination = false );
+        Dictionary<string, int> GetInfoStored();
+        Dictionary<string, int> GetStatistics();
+        void ResetStatistics();
+        void EmptyStored();
+
+        event Action<(int denomination, string currency, int codecurrency)> CoinDispensed;
+    }
+}
