5d9347e591d30d06583bd9c3da061f9a27c43da6
magals <itengineer@live.ru>
2024-08-05 03:17:52 +0300

    update: nuget

--- src/Magals.DevicesControl.SDKStandart/Interfeces/IBankTerminal.cs
+++ src/Magals.DevicesControl.SDKStandart/Interfeces/IBankTerminal.cs
@@ -5,12 +5,12 @@ namespace Magals.DevicesControl.SDKStandart.Interfeces
 {
     public interface IBankTerminal : IDevice
     {
-        void Payment(long amount, int code_currency);
+        void Payment(long amount, int code_currency, TypePayments typePayments = TypePayments.CARD);
         void Refund(long amount);
         void Cancel(long amount);
         void CancelACD(long amount);
         void ZReport();
 
-        event Action<BankTerminalCommand, BankTerminalResultCommand, object> ActionResultCommand;
+        event Action<(BankTerminalCommand command, BankTerminalResultCommand resultCode, object resultObjects, string stringForPrint)> ActionResultCommand;
     }
 }
